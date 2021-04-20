c5e487b18a080634dbe8f8628e5f8290e04756f3
Yessith Estiven Herrera Sanjuan <yessith.hs@gmail.com>
2021-04-20 16:00:40 -0500

    modificacion de elementos en el archivo main.js para usar buenas practicas de desarrollo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,75 +1,78 @@
 /*  VARIABLES */
-const $button = document.getElementById("button");
-const $menu = document.getElementById("menu");
-const $link = document.querySelectorAll(".header-menu__list__item--link");
-const $line = document.querySelector(".header-menu__button__burger--line");
-var img = document.querySelectorAll(".origin-shop__carousel__slider");
-var dots = document.getElementsByClassName("dot");
+const $button = document.getElementById('button');
+const $menu = document.getElementById('menu');
+const $link = document.querySelectorAll('.header-menu__list__item--link');
+const $line = document.querySelector('.header-menu__button__burger--line');
 
 /* SHOW MENU */
-$button.addEventListener("click", () => {
-  $menu.classList.toggle("show");
-  $line.classList.toggle("active");
+$button.addEventListener('click', () => {
+  $menu.classList.toggle('show');
+  $line.classList.toggle('active');
 });
 
 /* HIDDEN MENU */
 $link.forEach((n) =>
-  n.addEventListener("click", () => {
-    $menu.classList.remove("show");
+  n.addEventListener('click', () => {
+    $menu.classList.remove('show');
   })
 );
 
 /* ACTIVAR EVENTO SCROLL TRANSITION */
-window.addEventListener("scroll", () => {
-  let $elements = document.querySelectorAll(".scroll-content");
-  let $screenSize = window.innerHeight;
+window.addEventListener('scroll', () => {
+  const $elements = document.querySelectorAll('.scroll-content');
+  const $sizeElements = $elements.length;
+  const $screenSize = window.innerHeight;
 
-  for (var i = 0; i < $elements.length; i++) {
+  for (let i = 0; i < $sizeElements; i++) {
     let $element = $elements[i];
 
     if ($element.getBoundingClientRect().top < $screenSize) {
-      $element.classList.add("visible");
+      $element.classList.add('visible');
     } else {
-      $element.classList.remove("visible");
+      $element.classList.remove('visible');
     }
   }
 });
 
 /* SLIDER SHOW */
-var slideIndex = 0;
+let slideIndex = 0;
+let img = document.querySelectorAll('.origin-shop__carousel__slider');
+let dots = document.getElementsByClassName('dot');
+const sizeImg = img.length;
+const sizeDot = dots.length;
 
-var slideShow = () => {
-  for (i = 0; i < img.length; i++) {
-    img[i].style.display = "none";
+const slideShow = () => {
+  for (let i = 0; i < sizeImg; i++) {
+    img[i].style.display = 'none';
   }
 
   slideIndex++;
 
-  if (slideIndex > img.length) {
+  if (slideIndex > sizeImg) {
     slideIndex = 1;
   }
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" activo", "");
+  for (let i = 0; i < sizeDot; i++) {
+    dots[i].className = dots[i].className.replace(' activo', '');
   }
 
-  img[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " activo";
+  img[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' activo';
 
   setTimeout(slideShow, 4000);
 };
 slideShow();
 
-var currentSlide = (n) => {
-  for (i = 0; i < img.length; i++) {
-    img[i].style.display = "none";
+const currentSlide = (n) => {
+  for (let i = 0; i < sizeImg; i++) {
+    img[i].style.display = 'none';
   }
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" activo", "");
+  for (let i = 0; i < sizeDot; i++) {
+    dots[i].className = dots[i].className.replace(' activo', '');
   }
 
   slideIndex = n;
-  img[n - 1].style.display = "block";
-  dots[slideIndex - 1].className += " activo";
+  img[n - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' activo';
 };
